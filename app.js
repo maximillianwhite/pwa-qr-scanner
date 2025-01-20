9c47fab21d354517d107b6085ab3124b5de80a6f
MaxW <maximillianwhite@gmail.com>
2025-01-20 12:56:09 +0200

    G2

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// Load the list of valid codes
-// let codes = [];
-// fetch("codes.json")
-//     .then(response => response.json())
-//     .then(data => codes = data)
-//     .catch(error => {
-//         console.error("Error loading codes:", error);
-//         document.getElementById("status").innerText = "❌ Failed to load codes.";
-//     });
-
 const html5QrCode = new Html5Qrcode("reader");
 
 function onScanSuccess(decodedText) {
   html5QrCode.stop();
   document.getElementById("status").innerText = "⏳ Waiting..."
 
-  // Replace 'your-webhook-id' with your actual Make webhook URL
   fetch("https://hook.eu2.make.com/ytl5sarw52y1dcq7mx9oilz6xj3a8snp", {
       method: "POST",
       headers: {
@@ -27,24 +16,35 @@ function onScanSuccess(decodedText) {
           const statusElement = document.getElementById("status");
           const decodedElement = document.getElementById("decoded");
           decodedElement.innerText = `${decodedText}`;
+          const toggleValue = document.querySelector(".toggle-switch input").checked ? "P" : "A"; // Determine the toggle state
+          const firstLetter = decodedText.charAt(0);
           if (data.isValid) {
-              // Valid code
-              statusElement.innerText = `✅ Valid: ${data.info}`;
+              
               switch (data.class) {
                   case "G":
                       document.body.style.backgroundColor = "#C1E1C1";
+                      statusElement.innerText = `✅ 🟩 - Guest: ${data.info}`;
                       break;
                   case "S":
                       document.body.style.backgroundColor = "#FFE599";
+                      statusElement.innerText = `✅ 🟨 - Speaker: ${data.info}`;
                       break;
                   case "E":
                       document.body.style.backgroundColor = "#A7C7E7";
+                      statusElement.innerText = `✅ 🟦 - Exhibitor: ${data.info}`;
                       break;
                   case "T":
-                        document.body.style.backgroundColor = "#8E7CC3";
-                        break;
+                      document.body.style.backgroundColor = "#8E7CC3";
+                      statusElement.innerText = `✅ 🟪 - TBT Staff: ${data.info}`;
+                      break;
                   default:
                       document.body.style.backgroundColor = "grey";
+              }
+
+              if (firstLetter == toggleValue){
+                document.getElementById("AMPM").innerText = "✅"
+              } else {
+                document.getElementById("AMPM").innerText = "❌❌❌"
               }
           } else {
               // Invalid code
@@ -66,6 +66,9 @@ function onScanSuccess(decodedText) {
 function startScanner() {
     document.body.style.backgroundColor = "white";
     document.getElementById("status").innerText = "🚀 Ready to scan!"
+    document.getElementById("decoded").innerText = "Awaiting Scan..."
+    document.getElementById("AMPM").innerText = "..."
+
 
     html5QrCode.start(
         { facingMode: "environment" }, // Rear camera if available
@@ -95,6 +98,8 @@ function startScanner() {
 // Stop the scanner
 function stopScanner() {
     document.body.style.backgroundColor = "white";
+    document.getElementById("decoded").innerText = "Awaiting Scan..."
+    document.getElementById("AMPM").innerText = "..."
     html5QrCode.stop().then(() => {
         console.log("Scanner stopped");
     }).catch(err => {
